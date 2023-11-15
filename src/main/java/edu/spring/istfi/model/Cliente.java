40a893e5ee5eee911954f35e73e053a893fd74d2
emanuel2398 <gonzagabriel428@gmail.com>
2023-11-15 12:44:34 -0300

    test clientes

--- src/main/java/edu/spring/istfi/model/Cliente.java
+++ src/main/java/edu/spring/istfi/model/Cliente.java
@@ -1,19 +1,36 @@
 package edu.spring.istfi.model;
 
+import javax.persistence.Entity;
 
+@Entity
 public class Cliente {
 
     private int id;
     private int DNI;
     private int CUIT;
+    private String Nombre;
+    private String Apellido;
     private String razonSocial;
 
+    private Direccion direccion;
+
     // Constructor
-    public Cliente(int id, int DNI, int CUIT, String razonSocial) {
+    public Cliente(int id, int DNI, int CUIT, String razonSocial,String Nombre,String Apellido ,Direccion direccion) {
         this.id = id;
         this.DNI = DNI;
         this.CUIT = CUIT;
         this.razonSocial = razonSocial;
+        this.Nombre=Nombre;
+        this.Apellido=Apellido;
+        this.direccion = direccion;
+    }
+
+    public void setDireccion(Direccion direccion) {
+        this.direccion = direccion;
+    }
+
+    public Direccion getDireccion() {
+        return direccion;
     }
 
     // Getters y Setters
@@ -47,5 +64,21 @@ public class Cliente {
 
     public void setRazonSocial(String razonSocial) {
         this.razonSocial = razonSocial;
+    }
+
+    public String getApellido() {
+        return Apellido;
+    }
+
+    public void setApellido(String apellido) {
+        Apellido = apellido;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String nombre) {
+        Nombre = nombre;
     }
 }
